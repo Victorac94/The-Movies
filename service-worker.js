7768fb861eae8b4ba16dc7226b2654dd8887ee30
Victor Antunez <victorac94@hotmail.com>
2018-12-17 02:22:07 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/The-Movies/precache-manifest.f7f1248903a46dfdd9b74a49aa1b1ca9.js"
+  "/The-Movies/precache-manifest.cff9f16cb59e4cf9c1a27f51853604b0.js"
 );
 
 workbox.clientsClaim();
