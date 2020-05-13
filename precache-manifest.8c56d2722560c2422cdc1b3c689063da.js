c392c519ee224c8c9f888eefd1b34386cae78591
Victor Antunez <victorac94@hotmail.com>
2020-05-14 01:10:00 +0200

    Updates

---
+++ precache-manifest.8c56d2722560c2422cdc1b3c689063da.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fe4369d5fd5cc40dc76741036857217",
+    "url": "/The-Movies/index.html"
+  },
+  {
+    "revision": "d5741d38b9ca1cc28b47",
+    "url": "/The-Movies/static/css/main.55aa3ef2.chunk.css"
+  },
+  {
+    "revision": "35f1fe05090949b94b66",
+    "url": "/The-Movies/static/js/2.9865b512.chunk.js"
+  },
+  {
+    "revision": "4138ba3a1b8f330abe371e40bcb6aadd",
+    "url": "/The-Movies/static/js/2.9865b512.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5741d38b9ca1cc28b47",
+    "url": "/The-Movies/static/js/main.8daedb8a.chunk.js"
+  },
+  {
+    "revision": "28f9826538ae1338c191",
+    "url": "/The-Movies/static/js/runtime-main.07ba7d08.js"
+  },
+  {
+    "revision": "d834c6246c434ed3afdda55f2238ab92",
+    "url": "/The-Movies/static/media/image-not-available.d834c624.jpg"
+  },
+  {
+    "revision": "3ee45b2de686ef735f9a3dffa78739de",
+    "url": "/The-Movies/static/media/the-movies.3ee45b2d.woff"
+  },
+  {
+    "revision": "49f2a77a66559a3174acd9f743b07a40",
+    "url": "/The-Movies/static/media/the-movies.49f2a77a.ttf"
+  },
+  {
+    "revision": "e40ecbed422aa3ed719ba97b2585f7e0",
+    "url": "/The-Movies/static/media/the-movies.e40ecbed.svg"
+  },
+  {
+    "revision": "f1e98d7ce26eaeccddc6999341fe76a8",
+    "url": "/The-Movies/static/media/the-movies.f1e98d7c.eot"
+  }
+]);
